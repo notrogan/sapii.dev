6437d3e0109de5547d2d3d947bd27e02bf68400b
FBIGlowie <144389458+FBIGlowie@users.noreply.github.com>
2024-11-13 12:43:35 -0500

    added a dev flake installing hugo

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Hugo development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.hugo ];
+          shellHook = ''
+            echo "Hugo version: $(hugo version)"
+          '';
+        };
+      }
+    );
+}
+
